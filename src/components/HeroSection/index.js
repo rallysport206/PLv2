f1beb8999a18c902a1b39ad29b2582663891f471
rallysport206 <phan.tony125@gmail.com>
2020-11-07 22:15:27 -0800

    refactored css

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,27 +1,18 @@
 import React from 'react';
 import Video from '../../videos/video.mp4';
-import {
-    HeroContainer, 
-    HeroBackground,
-    HeroVideo,
-    HeroContent,
-    HeroCompany,
-    CompanyP
-} from './HeroElements';
+import './heroStyling.css';
 
 const HeroSection = () => {
     return (
-        <>
-            <HeroContainer>
-                <HeroBackground>
-                    <HeroVideo autoPlay loop muted src={Video} type='video/mp4' />
-                </HeroBackground>
-                <HeroContent>
-                    <HeroCompany>Paint Logic</HeroCompany>
-                    <CompanyP>Offering premium quality paint restoration in the Seattle area for over 15 years!</CompanyP>
-                </HeroContent>
-            </HeroContainer>
-        </>
+        <div className='hero-container'>
+            <div className='background'>
+                <video autoPlay loop muted src={Video} type='video/mp4' />
+            </div>
+            <div className='content'>
+                <h1>Paint Logic</h1>
+                <p>Offering premium quality paint restoration in the Seattle area for over 15 years!</p>
+            </div>
+        </div>
     )
 }
 
--- src/components/Info/index.js
+++ src/components/Info/index.js
@@ -1,44 +1,30 @@
 import React from 'react';
-import{
-    InfoContainer,
-    InfoWrapper,
-    InfoRow,
-    Column1,
-    Column2,
-    TextWrapper,
-    TopLine,
-    Heading,
-    Subtitle,
-    ImgWrap,
-    Img
-} from './InfoElements'
+import './infoStyling.css';
 
 
 const Info = ({lightBg, id, imgStart, headline, lightText, darkText, description, img, alt, 
 topLine
 }) => {
     return(
-        <>
-            <InfoContainer lightBg={lightBg} id={id}>
-                <InfoWrapper>
-                    <InfoRow imgStart={imgStart}>
-                        <Column1>
-                        <TextWrapper>
-                            <TopLine>{topLine}</TopLine>
-                            <Heading lightText={lightText}>{headline}</Heading>
-                            <Subtitle darkText={darkText}>{description}</Subtitle>
-                        </TextWrapper>
-                        </Column1>
-                        <Column2>
-                            <ImgWrap>
-                            <Img src={img} alt={alt}/>
-                            </ImgWrap>
-                        </Column2>
-                    </InfoRow>
-                </InfoWrapper>
-            </InfoContainer>
-        </>
-    )
+        <div className='info-container' lightBg={lightBg} id={id}>
+            <div className='info-wrapper'>
+                <div className='info-row' imgStart={imgStart}>
+                    <div className='column1'>
+                    <div className='text-wrapper'>
+                        <p className="topline">Offering premium quality paint restoration in the Seattle area for over 15 years!</p>
+                        <h2>Providing paint and minor body repairs at a lower cost than a body shop with the same high quality and attention to detail. Services include: paint repair from door dings, bumper scuffs, rock chip/road debris removal, curbed wheel repair, insurance claims, and more!</h2>
+                        <p className='subtitle' darkText={darkText}>Guaranteed Quick Service – Free Estimates – Most Repairs Take One Day!</p>
+                    </div>
+                    </div>
+                    <div className='column2'>
+                        <div className='img-wrap'>
+                        <ing src='../../images/car.png' alt='Shop Front'/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+)
 };
 
 export default Info;

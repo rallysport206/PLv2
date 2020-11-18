3d35b0975852857964f2df3a1da04fcd615a12bd
rallysport206 <phan.tony125@gmail.com>
2020-11-17 19:50:23 -0800

    updated img in cards. + break time

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -8,26 +8,32 @@ const Services = () => {
                 <div className='service-intro'>Our Services</div>
                 <div className='service-wrapper'>
                     <div className='service-card'>
+                        <div className='service-icons'></div>
                         <h4 className='card-headline'>Rock Chip Repair</h4>
                         <p className='card-text'>Repair and filling of paint chips and the removal of minor scratches</p>
                     </div>
                     <div className='service-card'>
+                        <div className='service-icons'></div>
                         <h4 className='card-headline'>Bumper Repair</h4>
                         <p className='card-text'>Repair and repaint bumper like new! Includes primer, wet sand, custom industry color match and layers of clear coat</p>
                     </div>
                     <div className='service-card'>
+                        <div className='service-icons'></div>
                         <h4 className='card-headline'>Wheel Repair and Repaint</h4>
                         <p className='card-text'>Repair and resurface wheel from damage due to: curbed wheels, dings, and other marks from road debris</p>
                     </div>
                     <div className='service-card'>
+                        <div className='service-icons'></div>
                         <h4 className='card-headline'>Plastic Bumper Welding</h4>
                         <p className='card-text'>Repair and repaint any cracks. Stronger than epoxy. Includes primer, wet sand, custom industry standard color match and layers of clear coat</p>
                     </div>
                     <div className='service-card'>
+                        <div className='service-icons'></div>
                         <h4 className='card-headline'>Insurance Bids</h4>
                         <p className='card-text'>We do insurance jobs and review jobs on a case by case basis. Stop by for an estimate</p>
                     </div>
                     <div className='service-card'>
+                        <div className='service-icons'></div>
                         <h4 className='card-headline'>Spot Blend Paint</h4>
                         <p className='card-text'>Repair of small portions of panels: bumpers, doors, hoods, fenders, quarter panels, roof, mirrors, trim</p>
                     </div>

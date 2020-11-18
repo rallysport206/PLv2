f86deeab95de7b90baf6a10f6471d42d228a218a
rallysport206 <phan.tony125@gmail.com>
2020-11-17 23:32:45 -0800

    updated card and info secions

--- src/components/Info/index.js
+++ src/components/Info/index.js
@@ -4,14 +4,18 @@ import './infoStyling.css';
 
 const Info = () => {
     return(
-        <div className='info-section__container'>
-            <div className='info-section__photo' />
-            <div className='info-section__content'>
-                <p className="info-section__topline">Offering premium quality paint restoration in the Seattle area for over 15 years!</p>
-                <h2 className='info-section__headline'>Providing paint and minor body repairs at a lower cost than a body shop with the same high quality and attention to detail. Services include: paint repair from door dings, bumper scuffs, rock chip/road debris removal, curbed wheel repair, insurance claims, and more!</h2>
-                <p className='info-section__subtitle'>Guaranteed Quick Service – Free Estimates – Most Repairs Take One Day!</p>
+        <>
+            <div className='info-section__container'>
+                <div className='info-section__wrapper'>
+                    <img className='info-section__photo' src='https://freesvg.org/img/sylwetka-polski_fiat_125.png' alt='car' />
+                    <div className='info-section__content'>
+                        <p className="info-section__topline">Offering premium quality paint restoration in the Seattle area for over 15 years!</p>
+                        <h2 className='info-section__headline'>Providing paint and minor body repairs at a lower cost than a body shop with the same high quality and attention to detail. Services include: paint repair from door dings, bumper scuffs, rock chip/road debris removal, curbed wheel repair, insurance claims, and more!</h2>
+                        <p className='info-section__subtitle'>Guaranteed Quick Service – Free Estimates – Most Repairs Take One Day!</p>
+                    </div>
+                </div>
             </div>
-        </div>
+        </>    
     )
 };
 
--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -9,32 +9,32 @@ const Services = () => {
                 <div className='service-wrapper'>
                     <div className='service-card'>
                         <div className='service-icons'></div>
-                        <h4 className='card-headline'>Rock Chip Repair</h4>
+                        <h3 className='card-headline'>Rock Chip Repair</h3>
                         <p className='card-text'>Repair and filling of paint chips and the removal of minor scratches</p>
                     </div>
                     <div className='service-card'>
                         <div className='service-icons'></div>
-                        <h4 className='card-headline'>Bumper Repair</h4>
+                        <h3 className='card-headline'>Bumper Repair</h3>
                         <p className='card-text'>Repair and repaint bumper like new! Includes primer, wet sand, custom industry color match and layers of clear coat</p>
                     </div>
                     <div className='service-card'>
                         <div className='service-icons'></div>
-                        <h4 className='card-headline'>Wheel Repair and Repaint</h4>
+                        <h3 className='card-headline'>Wheel Repair and Repaint</h3>
                         <p className='card-text'>Repair and resurface wheel from damage due to: curbed wheels, dings, and other marks from road debris</p>
                     </div>
                     <div className='service-card'>
                         <div className='service-icons'></div>
-                        <h4 className='card-headline'>Plastic Bumper Welding</h4>
+                        <h3 className='card-headline'>Plastic Bumper Welding</h3>
                         <p className='card-text'>Repair and repaint any cracks. Stronger than epoxy. Includes primer, wet sand, custom industry standard color match and layers of clear coat</p>
                     </div>
                     <div className='service-card'>
                         <div className='service-icons'></div>
-                        <h4 className='card-headline'>Insurance Bids</h4>
+                        <h3 className='card-headline'>Insurance Bids</h3>
                         <p className='card-text'>We do insurance jobs and review jobs on a case by case basis. Stop by for an estimate</p>
                     </div>
                     <div className='service-card'>
                         <div className='service-icons'></div>
-                        <h4 className='card-headline'>Spot Blend Paint</h4>
+                        <h3 className='card-headline'>Spot Blend Paint</h3>
                         <p className='card-text'>Repair of small portions of panels: bumpers, doors, hoods, fenders, quarter panels, roof, mirrors, trim</p>
                     </div>
                 </div>
